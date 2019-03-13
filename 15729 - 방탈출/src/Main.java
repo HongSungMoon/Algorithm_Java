2614e862261cc3fec9bb53558e87e90f512945af
Seongmun-Hong <hsm63746244@gmail.com>
2019-03-13 18:58:46 +0900

    fix :  - 방탈출 오류 수정

--- 15729 - 방탈출/src/Main.java
+++ 15729 - 방탈출/src/Main.java
@@ -3,9 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Main {
 
@@ -17,147 +15,32 @@ public class Main {
 		int length = Integer.parseInt(br.readLine());
 		String str = br.readLine().replaceAll(" ", "");
 
-		List<String> strArr = new ArrayList<String>();
+		boolean[] result = new boolean[str.length() + 2];
+		boolean[] target = new boolean[str.length() + 2];
+
+		Arrays.fill(result, false);
+		Arrays.fill(target, false);
 
 		for (int i = 0; i < str.length(); i++) {
-			strArr.add(str.charAt(i) + "");
-		}
-
-		if (length == 1 || length == 2) {
-			int returnValue = 0;
-			switch (str) {
-			case "1":
-				returnValue = 1;
-				break;
-			case "10":
-				returnValue = 2;
-				break;
-			case "11":
-				returnValue = 1;
-				break;
-			case "01":
-				returnValue = 1;
-				break;
+			if (str.charAt(i) == '1') {
+				target[i] = true;
 			}
-			bw.write(String.valueOf(returnValue));
-			bw.flush();
-			bw.close();
-			return;
 		}
 
-		boolean[] change = new boolean[2];
-		Arrays.fill(change, false);
-
-		// System.out.println(str);
-
-		int count = 0;
 		int idx = 0;
+		int count = 0;
 
 		while (true) {
-
-			String block = strArr.get(idx) + strArr.get(idx + 1) + strArr.get(idx + 2);
-			// System.out.println(block);
-			idx += 3;
-
-			switch (block) {
-			case "100":
-				count += 2;
-				change[0] = true;
-				break;
-			case "010":
-				count += 2;
-				change[1] = true;
-				break;
-			case "001":
-				count += 1;
-				change[0] = true;
-				change[1] = true;
-				break;
-			case "110":
-				count += 2;
-				change[0] = true;
-				break;
-			case "101":
-				count += 3;
-				change[1] = true;
-				break;
-			case "011":
-				count += 1;
-				change[0] = true;
-				break;
-			case "111":
-				count += 1;
+			if (idx == length) {
 				break;
 			}
-			int tmp_idx = 0;
-			if (length - idx < 3) {
-				if (!(change[0] == false && change[1] == false)) {
-					for (int i = 0; i < length - idx; i++) {
-						String tmp = null;
-						if (change[i]) {
-							tmp = "1";
-							if (!strArr.get(idx + i).equals(tmp)) {
-								if (strArr.get(idx + i).equals("0")) {
-									strArr.set(idx + i, "1");
-								} else {
-									strArr.set(idx + i, "0");
-								}
-							} else {
-								tmp_idx++;
-							}
-						}
-					}
-
-					idx += tmp_idx;
-				}
-
-				String lastStr = "";
-
-				for (int i = idx; i < length - 1; i++) {
-					lastStr += strArr.get(i);
-				}
-
-				switch (lastStr) {
-				case "1":
-					count += 1;
-					break;
-				case "01":
-					count += 1;
-					break;
-				case "10":
-					count += 2;
-					break;
-				case "11":
-					count += 1;
-					break;
-				}
-				break;
-			} else {
-				if (!(change[0] == false && change[1] == false)) {
-					for (int i = 0; i < 2; i++) {
-						String tmp = null;
-						if (change[i]) {
-							tmp = "1";
-							if (!strArr.get(idx + i).equals(tmp)) {
-								if (strArr.get(idx + i).equals("0")) {
-									strArr.set(idx + i, "1");
-								} else {
-									strArr.set(idx + i, "0");
-								}
-							} else {
-								tmp_idx++;
-							}
-						}
-					}
-
-					idx += tmp_idx;
-				}
+			if (result[idx] != target[idx]) {
+				count++;
+				result[idx + 2] = !result[idx + 2];
+				result[idx + 1] = !result[idx + 1];
 			}
-
-			Arrays.fill(change, false);
-
+			idx++;
 		}
-
 		bw.write(String.valueOf(count));
 
 		bw.flush();
