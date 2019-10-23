b5c0a35840458ee3e9d80033104a30e9e71ab944
Seongmun-Hong <hsm63746244@gmail.com>
2019-10-23 14:49:23 +0900

    add : 15596 - 정수 N개의 합 추가

---
+++ 15596 - 정수 N개의 합/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    long sum(int[] a) {
+        long ans = 0;
+        int size = a.length;
+        for (int i=0; i<size; i++) {
+            ans += a[i];
+        }
+        return ans;
+    }
+
+}
