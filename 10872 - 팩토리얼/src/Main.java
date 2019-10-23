171f5513b3ff56a89b9d5e92d7fc3343c9ceac85
Seongmun-Hong <hsm63746244@gmail.com>
2019-10-23 15:28:52 +0900

    add : 10872 - 팩토리얼 추가

---
+++ 10872 - 팩토리얼/src/Main.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int target = Integer.parseInt(br.readLine());
+        int result = 1;
+
+        for(int i=target; i>1; i--) {
+            result *= i;
+        }
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+
+    }
+}
