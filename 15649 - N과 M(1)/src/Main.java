ae69ddc864270c4e03a98abbf0b927a42059e0a6
Seongmun-Hong <hsm63746244@gmail.com>
2019-11-19 15:14:52 +0900

    fix : 15649 성능 향상

--- 15649 - N과 M(1)/src/Main.java
+++ 15649 - N과 M(1)/src/Main.java
@@ -1,6 +1,42 @@
 import java.io.*;
-import java.util.Stack;
+import java.util.StringTokenizer;
 
+public class Main {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static int[] perm;
+    private static boolean[] checked;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        checked = new boolean[n + 1];
+        perm = new int[m];
+        dfs(0);
+        bw.flush();
+    }
+
+    private static void dfs(int idx) throws IOException{
+        if (idx == perm.length) {
+            for (int p : perm) {
+                bw.write(p + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+        for (int i = 1; i < checked.length; i++) {
+            if (!checked[i]) {
+                checked[i] = true;
+                perm[idx] = i;
+                dfs(idx + 1);
+                checked[i] = false;
+            }
+        }
+    }
+}
+
+/*
 public class Main {
 
     public static Stack<Integer> stack = new Stack<>();
@@ -51,3 +87,5 @@ public class Main {
     }
 
 }
+*/
+
